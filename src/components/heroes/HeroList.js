99db2ef2b1ec65e130b753b35e8184329caadf35
Bryan Rodriguez <bsteven1993@gmail.com>
2021-08-09 15:34:29 -0500

    HeroCard added

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher';
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
 
     const heroes = getHeroesByPublisher(publisher);
 
     return (
-        <ul>
+        <div className="card-columns">
             {
-                heroes.map( heroe => (
-                    <li key={heroe.id}>
-                        { heroe.superhero }
-                    </li>
+                heroes.map( hero => (
+                    <HeroCard 
+                        key={hero.id}
+                        { ...hero }
+                    />
                 ))
             }
-        </ul>
+        </div>
     )
 }
